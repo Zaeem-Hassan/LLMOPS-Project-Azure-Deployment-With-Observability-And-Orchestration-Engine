6bac0966cda00559ccf3ee71da7d827d79c4d6d1
Zaeem Hassan <zaeemhassan2004@gmail.com>
2026-02-23 22:41:17 +0500

    Workflow build

---
+++ backend/src/graph/workflow.py
@@ -0,0 +1,26 @@
+from langgraph.graph import END, StateGraph
+from backend.src.graph.state import VideoAuditState
+
+from backend.src.graph.nodes import(
+    index_video_node,
+    audit_content_node
+)
+
+def create_graph():
+    
+
+    workflow = StateGraph(VideoAuditState)
+
+    workflow.add_node("indexor",index_video_node)
+    workflow.add_node("auditor",audit_content_node)
+
+    workflow.set_entry_point("indexer")
+
+    workflow.add_edge("indexor","auditor")
+
+    workflow.add_edge("auditor",END)
+
+    app = workflow.compile()
+    return app
+
+app  = create_graph()
